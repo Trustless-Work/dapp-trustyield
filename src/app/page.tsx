9e969af7314f427dd8dab59b65a86e62cf65045d
JoelVR17 <vermudo.com@gmail.com>
2025-09-16 08:18:59 -0600

    feat: fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <header className="flex justify-between items-center w-full">
-        <h2 className="text-2xl font-bold">Trustless Work</h2>
+        <h2 className="text-2xl font-bold">Trust Yield</h2>
         <div className="flex items-center gap-4">
           <ThemeToggle />
           <WalletButton />
